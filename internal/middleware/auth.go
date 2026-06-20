b2c9432664389b3848e2c5525bdd94f167761662
ashleybeloved <alphatripped+github@gmail.com>
2026-06-20 13:26:23 +0300

    feat: auth middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"forma/internal/config"
+	"forma/internal/pkg"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(cfg *config.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenStr, err := c.Cookie("forma_token")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+			return
+		}
+
+		claims, err := pkg.ValidateToken(tokenStr, cfg.JWTSecretKey)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired token"})
+			return
+		}
+
+		c.Set("user_id", claims.UserID)
+		c.Next()
+	}
+}
