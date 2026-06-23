d39da4f4b4ffb78187e72a8b11ed50b31b51832e
ashleybeloved <alphatripped+github@gmail.com>
2026-06-23 22:25:38 +0300

    fix: add /api group to all handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -39,6 +39,7 @@ func main() {
 		AllowOrigins: []string{
 			"http://localhost:3000", // For Dev
 			"https://" + cfg.Domain, // For Prod
+			"http://" + cfg.Domain,
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
@@ -64,32 +65,35 @@ func main() {
 	pollHandler := handler.NewPollHandler(pollService, cfg)
 
 	// - Routes -
-	// -- No Auth --
-	r.GET("/ping", pingHandler.Handle)
-	r.POST("/register", userHandler.Register)
-	r.POST("/login", userHandler.Login)
-	r.POST("/logout", userHandler.Logout)
-
-	// -- Guest Token Required --
-	guest := r.Group("")
-	guest.Use(middleware.GuestMiddleware(cfg))
+	api := r.Group("/api")
 	{
-		guest.GET("/poll/:short_id", pollHandler.GetPollByShortID) // Get Poll
-		guest.POST("/poll/:short_id/vote", pollHandler.Vote)       // Vote in Poll
-		guest.POST("/poll/:short_id/check", pollHandler.CheckVote) // Check vote user
-	}
+		// -- No Auth --
+		api.GET("/ping", pingHandler.Handle)
+		api.POST("/register", userHandler.Register)
+		api.POST("/login", userHandler.Login)
+		api.POST("/logout", userHandler.Logout)
 
-	// -- Need Auth --
-	auth := r.Group("")
-	auth.Use(middleware.AuthMiddleware(cfg))
-	{
-		auth.GET("/me", userHandler.Me)
-		auth.POST("/poll", pollHandler.CreatePoll)   // Create Poll
-		auth.PATCH("/poll", pollHandler.UpdatePoll)  // Edit Poll
-		auth.DELETE("/poll", pollHandler.DeletePoll) // Delete Poll
-		auth.GET("/poll", pollHandler.GetAllMyPolls) // Get All Profile Polls | Queries LIMIT & OFFSET
+		// -- Guest Token Required --
+		guest := api.Group("")
+		guest.Use(middleware.GuestMiddleware(cfg))
+		{
+			guest.GET("/poll/:short_id", pollHandler.GetPollByShortID) // Get Poll
+			guest.POST("/poll/:short_id/vote", pollHandler.Vote)       // Vote in Poll
+			guest.POST("/poll/:short_id/check", pollHandler.CheckVote) // Check vote user
+		}
 
-		auth.GET("/poll/:short_id/stats", pollHandler.GetPollStats) // Statistics
+		// -- Need Auth --
+		auth := api.Group("")
+		auth.Use(middleware.AuthMiddleware(cfg))
+		{
+			auth.GET("/me", userHandler.Me)
+			auth.POST("/poll", pollHandler.CreatePoll)   // Create Poll
+			auth.PATCH("/poll", pollHandler.UpdatePoll)  // Edit Poll
+			auth.DELETE("/poll", pollHandler.DeletePoll) // Delete Poll
+			auth.GET("/poll", pollHandler.GetAllMyPolls) // Get All Profile Polls | Queries LIMIT & OFFSET
+
+			auth.GET("/poll/:short_id/stats", pollHandler.GetPollStats) // Statistics
+		}
 	}
 
 	// -!- Start server & Graceful Shutdown -!-
